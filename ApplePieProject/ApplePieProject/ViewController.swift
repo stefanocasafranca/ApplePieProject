ff66230da0111270fc7176ad23ede0c7aca2fb21
Stefano Casafranca <scasafrancal01@gmail.com>
2024-09-24 20:54:50 -0500

    Pictures and Device Set up

---
+++ ApplePieProject/ApplePieProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePieProject
+//
+//  Created by Stefano Casafranca Laos on 9/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //Apple Pie Project
+    }
+
+
+}
+
