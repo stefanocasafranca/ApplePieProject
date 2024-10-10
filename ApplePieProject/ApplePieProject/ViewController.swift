f334c660d4da5866f86fe3dc8aebae23de0f361f
stefanocasafranca <scasafrancal01@gmail.com>
2024-10-09 19:20:53 -0500

    Part 1 - Connecting Images and Buttons through Outlets and Actions

--- ApplePieProject/ApplePieProject/ViewController.swift
+++ ApplePieProject/ApplePieProject/ViewController.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        
+        sender.isEnabled = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
